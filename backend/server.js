141a907df78431f0cdc4825eeabee4e943859ff7
abdulwahabahmedkhanyusufzai <absurw@gmail.com>
2024-12-31 16:42:47 +0500

    comitted

--- backend/server.js
+++ backend/server.js
@@ -40,13 +40,7 @@ app.use("/api/v1/movie", protectRoute, movieRoutes);
 app.use("/api/v1/tv", protectRoute, tvRoutes);
 app.use("/api/v1/search", protectRoute, searchRoutes);
 
-if (ENV_VARS.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
-}
 
   app.get("/", (req, res) => {
     res.status(404).json({ message: "API route found" });
